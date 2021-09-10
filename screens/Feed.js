112377198e29723bd84ecc157432dd79c1b31f71
san994 <75426313+san994@users.noreply.github.com>
2021-09-10 00:10:46 -0700

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View, FlatList, SafeAreaView, Image,Platform,StatusBar,StyleSheet} from 'react-native';
+
+
+export default class Feed extends Component {
+
+    render() {
+        return (
+           <Text>Feed</Text>
+        )
+    }
+}
+
+
